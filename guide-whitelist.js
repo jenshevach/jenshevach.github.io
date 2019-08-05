c84fc02211701df3de858b91b0f8b388eb0521de
Jennifer Shevach <jennifer@pendo.io>
2019-08-05 13:33:38 +0300

    update options

--- guide-whitelist.js
+++ guide-whitelist.js
@@ -4,9 +4,11 @@ function basic_install(isUpdate) {
     var role = document.getElementById("myRole").value;
     var privateInfo = document.getElementById("myPrivateInfo").value;
     var email = document.getElementById("myEmail").value;
+    var newfield = document.getElementById("newfield").value;
+    var newfieldval = document.getElementById("newfieldval").value;
 
     if (isUpdate) {
-        pendo.updateOptions({
+        var updOpts = {
             visitor: {
                 id: visitor,   // Required if user is logged in
                 role: role,
@@ -16,9 +18,11 @@ function basic_install(isUpdate) {
             account: {
                 id: account // Highly recommended
             }
-        });
+        };
+        updOpts.account[newfield] = newfieldval;
+        pendo.updateOptions(updOpts);
     } else {
-        pendo.initialize({
+        var initOpts = {
             apiKey: "358d840f-d7f9-4999-4d02-fffb827c114a",
             visitor: {
                 id: visitor,   // Required if user is logged in
@@ -29,36 +33,8 @@ function basic_install(isUpdate) {
             account: {
                 id: account // Highly recommended
             }
-        });
-
-    }
-}
-
-function secure_install(isUpdate) {
-    var visitor = document.getElementById("myVisitor").value;
-    var account = document.getElementById("myAccount").value;
-    var role = document.getElementById("myRole").value;
-    var privateInfo = document.getElementById("myPrivateInfo").value;
-    var email = document.getElementById("myEmail").value;
-    var token = generate_jwt(visitor, account, role, privateInfo, email);
-
-    if (isUpdate) {
-        pendo.updateOptions({
-            visitor: {
-                id: token   // Required if user is logged in
-            },
-            account: {
-                id: "secure_snippet" // Highly recommended
-            }
-        });
-    } else {
-        pendo.initialize({
-            visitor: {
-                id: token   // Required if user is logged in
-            },
-            account: {
-                id: "secure_snippet" // Highly recommended
-            }
-        });
+        };
+        initOpts.account[newfield] = newfieldval;
+        pendo.initialize(initOpts);
     }
 }
